951fa8af19ca697867fc5613ac4f27553e8003cd
jmachuca77 <jaime@element.aero>
2019-12-11 19:43:23 -0700

    add open cv

--- src/node_detectnet.cpp
+++ src/node_detectnet.cpp
@@ -30,6 +30,10 @@
 #include <jetson-utils/cudaMappedMemory.h>
 
 #include <image_transport/image_transport.h>
+#include <opencv2/core.hpp>
+#include <opencv2/imgproc/imgproc.hpp>
+#include <opencv2/highgui/highgui.hpp>
+#include <cv_bridge/cv_bridge.h>
 
 #include "image_converter.h"
 
